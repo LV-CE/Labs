aafa5e440d17a51a0c2733209cf914fff548dc2c
LV-CE <lololol20062006@gmail.com>
2025-06-02 22:08:30 +0300

    Delete 6 directory

--- 6/src/main/repository/Repository.java
+++
@@ -1,8 +0,0 @@
-package main.repository;
-import main.resource.Product;
-import java.util.List;
-
-public interface Repository {
-    void save(List<Product> products);
-    List<Product> load();
-}
